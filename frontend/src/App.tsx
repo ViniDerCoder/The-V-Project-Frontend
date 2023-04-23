9779ae7e5d6ad67ca21cdeb69f28dab1e8f43876
ViniDerCoder <85809337+ViniDerCoder@users.noreply.github.com>
2023-04-23 11:32:39 +0200

    Test of different resizing methods

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box } from '@mui/material';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Box sx={{position: "relative", width: "90%", height: "90vh", backgroundColor: "red", left: "50%", transform: "translateX(-50%)"}}></Box>
+      <Box sx={{position: "relative", width: "90%", height: "90vh", backgroundColor: "blue", left: "50%", transform: "translateX(-50%)"}}></Box>
+    </>
+    );
 }
