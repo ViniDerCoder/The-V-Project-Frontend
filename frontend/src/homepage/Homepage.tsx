18172999d4fe14f98224b55944b021f99a967423
coder-198 <vincent.baechle@icloud.com>
2023-04-24 19:38:36 +0200

    alles kaputt jetzt nicht mehr

--- frontend/src/homepage/Homepage.tsx
+++ frontend/src/homepage/Homepage.tsx
@@ -7,35 +7,28 @@ import MenuUserAndButton from './MenuUserAndButton'
 import { ColorThemeInterface } from '../App'
 import { useLocation, useNavigate } from 'react-router-dom'
 
-export interface ColorThemeInterface{
-    first: string, 
-    second: string,
-    third: string,
-    fourth: string
+export interface Props{
+    colorTheme: ColorThemeInterface
 }
 export default function Homepage(props: Props) {
+    const [sidebarActive, setSidebarActive] = useState(false)
     const navigation = useNavigate()
     const location = useLocation()
 
     //Send to correct webpage if no path is defined
     useEffect(() => location.pathname === '/' ? navigation('/web/home') : undefined)
 
-export default function Homepage() {
-    const [colorTheme, setColorTheme] = useState<ColorThemeInterface>({first: "#3c3f4d", second: "#e8b343", third: "#b75957", fourth: "#f8ffea"})
-    const [sidebarActive, setSidebarActive] = useState(false)
-
     const handleMenuButtonClick = () => {
         setSidebarActive(!sidebarActive)
     }
 
   return (
-    <Box sx={{height: "100vh"}}>
-        <Box sx={{position: "sticky"}}>
-            <Headbar colorTheme={colorTheme}/>
-            <MenuUserAndButton colorTheme={colorTheme} handleMenuButtonClick={handleMenuButtonClick}/>
-            <Sidebar active={sidebarActive} colorTheme={colorTheme}></Sidebar>
+    <Box sx={{height: "300vh", width: "100%", backgroundColor: props.colorTheme.fourth}}>
+        <Box className="sticky-homepage">
+            <Headbar colorTheme={props.colorTheme}/>
+            <MenuUserAndButton colorTheme={props.colorTheme} handleMenuButtonClick={handleMenuButtonClick}/>
+            <Sidebar active={sidebarActive} colorTheme={props.colorTheme}></Sidebar>
         </Box>
-        <Box sx={{width: "100%", height: "150vh", backgroundColor: colorTheme.fourth}}></Box>
     </Box>
   )
 }
