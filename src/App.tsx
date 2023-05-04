d9cf8a8a8167a30471b588c0197c55896a5910d0
ViniDerCoder <85809337+ViniDerCoder@users.noreply.github.com>
2023-05-04 17:10:11 +0200

    Changed Project Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './homepage/Homepage';
 import Settings from './settings/Settings';
