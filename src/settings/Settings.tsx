894358483937ff77d3a9a8f05df83438d204f22e
coder-198 <vincent.baechle@icloud.com>
2023-05-08 22:09:32 +0200

    added hombutton hover effect

--- src/settings/Settings.tsx
+++ src/settings/Settings.tsx
@@ -6,7 +6,8 @@ import Profile_Settings from "./pages/Profile_Settings"
 import { useState } from "react"
 import * as ReactRouter from "react-router-dom"
 
-import { HiOutlineHome as HomeIcon} from "react-icons/hi"
+import { AiOutlineHome as HomeIcon, AiFillHome as HomeFilledIcon } from "react-icons/ai"
+
 
 const SettingPages = [
     {title: "General", component: General, unsavedChanges: false},
@@ -21,6 +22,7 @@ export default function Settings(props: Props) {
     const navigation = ReactRouter.useNavigate()
 
     const [activePage = SettingPages.length, setActivePage] = useState(0)
+    const [homeHover, setHomeHover] = useState(false)
 
     const options = SettingPages.map((key, value) => {
         return(<Box className='sidebar-menu-selction-field' onClick={() => setActivePage(SettingPages.indexOf(key))} sx={{color: props.colorTheme.fourth, borderBottomColor: props.colorTheme.first, top: `${7*SettingPages.indexOf(key)+1}vh`}}>{key.title}</Box>)
@@ -30,7 +32,11 @@ export default function Settings(props: Props) {
         <Box sx={{height: "100vh", backgroundColor: props.colorTheme.fourth}}>
             <Box className='settings-title' sx={{backgroundColor: props.colorTheme.first, borderBottomColor: props.colorTheme.second, color: props.colorTheme.fourth}}>Settings</Box>
             <Box className='settings-action-title' sx={{color: props.colorTheme.first}}>{SettingPages[activePage].title}</Box>
-            <HomeIcon className='settings-home-button' color={props.colorTheme.first} onClick={() =>  {SettingPages.every(obj => !obj.unsavedChanges) ? navigation('/home') : showUnsavedChangesPopUp()}}/>
+            {!homeHover ? (
+                <HomeIcon className='settings-home-button' color={props.colorTheme.first} onMouseEnter={() => setHomeHover(true)} onClick={() =>  {SettingPages.every(obj => !obj.unsavedChanges) ? navigation('/home') : showUnsavedChangesPopUp()}}/>
+            ) : (
+                <HomeFilledIcon className='settings-home-button' color={props.colorTheme.first}  onMouseLeave={() => setHomeHover(false)} onClick={() =>  {SettingPages.every(obj => !obj.unsavedChanges) ? navigation('/home') : showUnsavedChangesPopUp()}}/>
+            )}
             <Box className='settings-action-slot'>
                 {SettingPages[activePage].component({colorTheme: props.colorTheme})}
             </Box>
