5d9249da01235c01641dc863f5984760c8737bed
coder-198 <vincent.baechle@icloud.com>
2023-05-11 18:24:32 +0200

    added homebutton to headbar

--- src/homepage/Headbar.tsx
+++ src/homepage/Headbar.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography } from '@mui/material'
 import { ColorThemeInterface } from '../App'
 import './homepage.css'
+import * as ReactRouter from "react-router-dom"
 
 interface Props{
     colorTheme: ColorThemeInterface
@@ -8,10 +9,12 @@ interface Props{
 
 export default function Headbar(props: Props) {
 
+  const navigation = ReactRouter.useNavigate()
+
   return (
     <>
         <Box className="header-bar" sx={{backgroundColor: props.colorTheme.first, alignItems: "center"}}>
-            <Typography variant='h3' sx={{left: "3vh", position: "absolute", color: props.colorTheme.second, fontFamily: "monospace"}}>The-V-Project</Typography>
+            <Typography variant='h3' sx={{left: "3vh", position: "absolute", color: props.colorTheme.second, fontFamily: "monospace"}} onClick={() => navigation('/home')}>The-V-Project</Typography>
         </Box>
     </>
   )
